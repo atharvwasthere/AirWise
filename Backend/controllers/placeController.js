cea71ac95595293de9472d1320372e64e621093d
Atharv <singhatharv1919@gmail.com>
2025-02-23 23:13:43 +0530

    Add Controllers

--- Backend/controllers/placeController.js
+++ Backend/controllers/placeController.js
@@ -7,3 +7,29 @@
         post location details 
 
         */  
+
+        const Place = require("../models/placeModel");
+        const { getStateFromCoordinates } = require("../utils/geocodeUtils");
+        const { fetchAQIData } = require("../services/aqiService");
+        
+        // Get hill stations in the user's state
+        const getHillStationsByState = async (req, res) => {
+          const { lat, lon } = req.query;
+          try {
+            const state = await getStateFromCoordinates(lat, lon);
+            const hillStations = await Place.find({ state });
+            res.json(hillStations);
+          } catch (error) {
+            res.status(500).json({ message: error.message });
+          }
+        };
+        
+        // Get AQI history for a hill station
+        const getAQIHistory = async (req, res) => {
+          try {
+            const place = await Place.findById(req.params.id);
+            res.json(place.aqiHistory);
+          } catch (error) {
+            res.status(500).json({ message: error.message });
+          }
+        };

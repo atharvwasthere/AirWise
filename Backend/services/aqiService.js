5cb9c14d6928a51048e2df0e051d83773f95ede3
Atharvwasthere <singhatharv1919@gmail.com>
2025-05-31 23:38:36 +0530

    refactor: improve AQI and geocoding services

--- Backend/services/aqiService.js
+++ Backend/services/aqiService.js
@@ -1,19 +1,29 @@
-const axios = require("axios");
-const redisClient = require("../config/redis");
+import { Axios } from "axios";
+import {redisClient} from "../config/redis.js"
 
 const fetchAQIData = async (lat, lon) => {
   const cacheKey = `aqi:${lat},${lon}`;
-  const cachedData = await redisClient.get(cacheKey);
-  if (cachedData) return JSON.parse(cachedData);
-
   try {
-    const response = await axios.get(
+    const cachedData = await redisClient.get(cacheKey);
+    if (cachedData) return JSON.parse(cachedData);
+
+  } catch (error) {
+    console.error("Error getting AQI data from cache",error.message);
+    // If there's an error with Redis, proceed to fetch from the API
+
+  }
+  // Now fetching from WAQI
+  try {
+    const response = await Axios.get(
       `https://api.waqi.info/feed/geo:${lat};${lon}/?token=${process.env.WAQI_TOKEN}`
     );
     const aqiData = response.data.data;
     await redisClient.set(cacheKey, JSON.stringify(aqiData), { EX: 3600 }); // Cache for 1h
     return aqiData;
   } catch (error) {
-    throw new Error("Failed to fetch AQI data");
+    console.error("Failed to fetch AQI data",error.message);
+    return{aqi: null , message: "Failed to fetch AQI data from WAQI"}
   }
 };
+
+module.exports = fetchAQIData ;
---
+++ Backend/services/geocodeService.js
@@ -0,0 +1,45 @@
+import { axios } from "axios";
+import {redisClient} from "../config/redis.js"
+
+// geting  States from user's current corrdinates
+
+const getIndianLocationDetails  = async (lat, lon) => {
+    const cacheKey = `geocode: ${lat},${lon}`;
+    try {
+    const cachedState = await redisClient.get(cacheKey);
+    if (cachedState) return JSON.parse(cachedState);
+    } catch(error) {
+        console.error("Error getting AQI data from cache",error.message);
+        // If there's an error with Redis, proceed to fetch from the API
+
+    }
+    // Fetch from nominatim
+    try{
+        const response = await axios.get("https://nominatim.openstreetmap.org/reverse" , {
+            params: { lat, lon, format: "json" , addressdetails: 1, country: "India"},
+        });
+        const state = normalizeStateName(response.data.address.state);
+
+        // chached for 2 weeks
+        await redisClient.set(cacheKey , state, { EX: 1209600 }); 
+        return state;
+    } catch (error) {
+        console.log("Nominatim API Error" , error.message);
+        throw new Error("Geocoding Failed: Could not determine state");
+    }
+};
+
+// not really req but will check later 
+const normalizeStateName = (State) =>{
+    const stateMapping = { 
+        'Delhi': 'National Capital Territory of Delhi',
+        'Bombay': 'Maharashtra',
+        // Add more mappings as needed
+    };
+    return stateMapping(State) || State;
+}
+
+module.exports = getIndianLocationDetails ;
+
+//redis failure => console.error
+//nominatim failure => throw error
